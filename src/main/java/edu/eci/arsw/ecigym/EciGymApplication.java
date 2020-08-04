ab96cb4fb98fc7f8594ef846bf5d3fe30dc6646f
Sebastian Frásica <juansebastianfrasica@hotmail.com>
2020-08-04 18:49:22 -0500

    first commit

---
+++ src/main/java/edu/eci/arsw/ecigym/EciGymApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ecigym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/ecigym/EciGymApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ecigym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
