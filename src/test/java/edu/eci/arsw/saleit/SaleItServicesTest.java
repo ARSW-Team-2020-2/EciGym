8e7ec828c28bfc16bc29a8b1dbd82443915af9fb
Juan Sebastián Frásica G <32890741+sebastianfrasic@users.noreply.github.com>
2020-08-28 14:40:40 -0500

    Arreglando Spring

---
+++ src/test/java/edu/eci/arsw/saleit/SaleItServicesTest.java
@@ -0,0 +1,17 @@
+package edu.eci.arsw.saleit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
+
+public class SaleItServicesTest {
+
+    @Test
+    public void doSomething(){
+
+    }
+}
