1990bdbdff4e4650897a0984d2457fc327ceb788
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2020-09-02 13:03:01 -0500

    Esqueleto app completo

--- src/main/java/edu/eci/arsw/saleit/model/Articulo.java
+++ src/main/java/edu/eci/arsw/saleit/model/Articulo.java
@@ -1,4 +1,135 @@
 package edu.eci.arsw.saleit.model;
 
 public class Articulo {
+    
+    private int id;
+    private int idAuction;
+    private String name;
+    private String use;
+    private String description;
+    private int minPrice;
+    private String dimensions;
+    private String location;
+    private String image;
+    private int idCategory;
+    private boolean isActive;
+
+    public Articulo(int id, int idAuction, String name, String use, String description, int minPrice, String dimensions, String location, String image, int idCategory) {
+        this.id = id;
+        this.idAuction = idAuction;
+        this.name = name;
+        this.use = use;
+        this.description = description;
+        this.minPrice = minPrice;
+        this.dimensions = dimensions;
+        this.location = location;
+        this.image = image;
+        this.idCategory = idCategory;
+        this.isActive = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdAuction() {
+        return idAuction;
+    }
+
+    public void setIdAuction(int idAuction) {
+        this.idAuction = idAuction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUse() {
+        return use;
+    }
+
+    public void setUse(String use) {
+        this.use = use;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(int minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Articulo{" + 
+                "id=" + id + 
+                ", idAuction=" + idAuction + 
+                ", name=" + name + 
+                ", use=" + use + 
+                ", description=" + description + 
+                ", minPrice=" + minPrice + 
+                ", dimensions=" + dimensions + 
+                ", location=" + location + 
+                ", image=" + image + 
+                ", idCategory=" + idCategory + '}';
+    }  
 }
--- src/main/java/edu/eci/arsw/saleit/model/Puja.java
+++ src/main/java/edu/eci/arsw/saleit/model/Puja.java
@@ -1,4 +1,60 @@
 package edu.eci.arsw.saleit.model;
 
+import java.util.Date;
+
 public class Puja {
+    
+    private int id;
+    private Usuario user;
+    private int amount;
+    private Date date;
+
+    public Puja(int id, Usuario user, int amount, Date date) {
+        this.id = id;
+        this.user = user;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Puja{" + 
+                "id=" + id + 
+                ", user=" + user + 
+                ", amount=" + amount + 
+                ", date=" + date + '}';
+    }  
+            
 }
