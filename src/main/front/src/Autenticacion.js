83a3e544163fb246e23e5d8bc4a3e14f9882cc00
angiedanielar <angie.ruiz@mail.escuelaing.edu.co>
2020-10-05 13:06:54 -0500

    front inicial completo

--- src/main/front/src/Autenticacion.js
+++ src/main/front/src/Autenticacion.js
@@ -4,27 +4,27 @@ import SignUp from './SignUp';
 
 function Autenticacion() {
     return (
-    <div className="container register auction-image">
-        <div className="container register">
-            <ul className="nav nav-tabs" id="myTab" role="tablist">
-            <li className="nav-item">
-                    <a className="nav-link" id="profile-tab" data-toggle="tab" href="TabRegistrarse" role="tab" aria-controls="profile" aria-selected="false">Sign up</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" id="home-tab" data-toggle="tab" href="TabIngresar" role="tab" aria-controls="home" aria-selected="true">Sign in</a>
-                </li>
+        <div className="container register auction-image">
+          <div className="container register">
+            <ul className="nav nav-tabs nav-justified" id="myTab" role="tablist">
+              <li className="nav-item">
+                <a className="nav-link" id="home-tab" data-toggle="tab" href="#TabSignIn" role="tab" aria-controls="home" aria-selected="false">Ingresar</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" id="profile-tab" data-toggle="tab" href="#TabSignUp" role="tab" aria-controls="profile" aria-selected="true">Registrarse</a>
+              </li>
             </ul>
             <div className="tab-content" id="myTabContent">
-                <div className="tab-pane fade fade show active" id="TabSignIn" role="tabpanel" aria-labelledby="home-tab">
-                    <SignUp> </SignUp>
-                </div>
-                <div className="tab-pane" id="TabSignUp" role="tabpanel" aria-labelledby="profile-tab">
-                    <SignIn> </SignIn>
-                </div>
+              <div className="tab-pane fade" id="TabSignIn" role="tabpanel" aria-labelledby="home-tab">
+                <SignIn></SignIn>
+              </div>
+              <div class="tab-pane fade show active" id="TabSignUp" role="tabpanel" aria-labelledby="profile-tab">
+                <SignUp></SignUp>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-    );
+      );
 }
 
 export default Autenticacion;
