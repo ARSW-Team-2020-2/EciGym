8bbd208299a61f9a4810771c0402e5aaaedf9b90
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2020-10-06 09:46:18 -0500

    Agregando pruebas SaleItServices

--- src/test/java/edu/eci/arsw/saleit/SaleItTest.java
+++ src/test/java/edu/eci/arsw/saleit/SaleItTest.java
@@ -1,6 +1,6 @@
 package edu.eci.arsw.saleit;
 
-import edu.eci.arsw.saleit.model.Subasta;
+import edu.eci.arsw.saleit.model.*;
 import edu.eci.arsw.saleit.services.SaleItServices;
 import edu.eci.arsw.saleit.services.SaleItServicesException;
 import org.junit.Test;
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.sql.Timestamp;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,5 +27,54 @@ public class SaleItTest {
         Subasta s = saleItServices.getAuctionByID(38);
         Timestamp timestamp = s.getFechaFin();
         assertEquals(timestamp.toString(), "2020-10-13 10:30:30.0");
+    }
+    
+    @Test
+    public void shouldGetCategoryId() throws SaleItServicesException {
+        Categoria c = saleItServices.getCategoryById(1);
+        String nombre = c.getNombre();
+        assertEquals(nombre, "Hogar y Cocina");
+    }
+    
+    @Test
+    public void shouldGetArticleId() throws SaleItServicesException {
+        Articulo a = saleItServices.getArticleById(35);
+        String nombre = a.getNombre();
+        assertEquals(nombre, "Reloj");
+    }
+    
+    @Test
+    public void shouldGetArticleOfAnAuction() throws SaleItServicesException {
+        Subasta s = saleItServices.getAuctionByID(38);
+        Articulo a = s.getArticulo();
+        String nombre = a.getNombre();
+        assertEquals(nombre, "Moto AKT");
+    }
+    
+    @Test
+    public void shouldNotGetNonExistentCategory() {
+        try {
+            saleItServices.getCategoryById(666);
+        } catch (SaleItServicesException e) {
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void shouldNotGetNonExistentAuction() {
+        try {
+            saleItServices.getAuctionByID(666);
+        } catch (SaleItServicesException e) {
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void shouldNotGetNonExistentArticle() {
+        try {
+            saleItServices.getArticleById(666);
+        } catch (SaleItServicesException e) {
+            assertTrue(true);
+        }
     }
 }
